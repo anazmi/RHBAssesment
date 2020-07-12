9f57681b1caaf96407cbe6a8050781146aad4ce7
anazmi <ikhadib@gmail.com>
2020-07-13 02:13:16 +0800

    Delete sql related file from h2

--- TestRHBSpringBoot/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-insert into users(userid,firstname, lastname, email)
-values('100', 'test', 'user', 'ikha@gmail.com');
-
-insert into users(userid,firstname, lastname, email)
-values('101', 'Adib', 'nazmi', 'test@test.com');
-
--- TestRHBSpringBoot/src/main/resources/schema.sql
+++
@@ -1,6 +0,0 @@
-create table users(
-	userid int auto_increment primary key,
-	firstName varchar2(50),
-	lastName varchar2(50),
-	email varchar2(50)
-		);
